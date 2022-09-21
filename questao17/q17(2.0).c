8c499faf476b69d99fdae7389d7587a4cf0a7e54
Maressa Dâmaris Lima Diógenes <50055001+MaressaD@users.noreply.github.com>
2022-09-21 08:29:36 -0300

    atualizarq17

---
+++ questao17/q17(2.0).c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void soma_vetor(int *vetor1,int *vetor2,int *vetor3,int size);
+
+int main(void) {
+  unsigned int n,i;
+  int *vetorA,*vetorB,*vetorR;
+
+    do{
+        printf("Qual tamanho dos vetores que deseja somar \n");
+        scanf("%d", &n);
+    }while(n<=0);
+
+    vetorA = malloc(n*sizeof(int));
+    vetorB = malloc(n*sizeof(int));
+    vetorR = malloc(n*sizeof(int));
+
+    for( i = 0; i < n; i++) {
+        printf("\nDigite o valor da posicao %d do vetorA: ", i);
+        scanf("%d", &vetorA[i]);
+    }
+    for( i = 0; i < n; i++) {
+        printf("\n\nDigite o valor da posicao %d do vetorB: ", i);
+        scanf("%d", &vetorB[i]);
+    }
+
+    soma_vetor(vetorA,vetorB,vetorR,n);
+
+    for(int j=0;j<n;j++){
+        printf("\n%d ", vetorR[j]);
+    }
+
+    free(vetorA);
+    free(vetorB);
+    free(vetorR);
+    return 0;
+}
+
+void soma_vetor(int *vetor1,int *vetor2,int *vetor3,int size){
+    int i;
+    for(i=0;i<size;i++){
+        vetor3[i] = vetor1[i] + vetor2[i];
+    }
+};
+
+
+
