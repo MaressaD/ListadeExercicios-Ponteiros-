081416695eb27fecda7632f96ef82f24deed223f
Maressa Dâmaris Lima Diógenes <maressadld@gmail.com>
2022-09-23 14:02:27 -0300

    faltam3

---
+++ questao12/q12.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma_vetor(int *vetor,int size);
+
+int main(void){
+    int nc,i,*custos,ctotal;
+    int (*pont_func)(int*,int); //ponteiro para função;
+    nc=5;
+    custos = malloc(nc*sizeof(int));
+
+    printf("digite cinco(5) valores de custo:\n");
+    for(i=0;i<nc;i++){
+        printf("digite o valor %d :\n",i+1);
+        scanf("%d",&custos[i]);
+    }
+    pont_func = soma_vetor; //ponteiro para função recebe a função soma_vetor
+    ctotal=pont_func(custos,nc); //o ponteiro para função pont_fun agora pode ser chamado sempre que se desejar usar a função soma_vetor
+
+    printf("O total de custos foi: %d",ctotal);
+
+    return 0;
+}
+
+int soma_vetor(int *vetor,int size){
+    int i,soma=0;
+    for(i=0;i<size;i++){
+        soma=soma+vetor[i];
+    }
+    return soma;
+}
--- questao15/q15.c
+++ questao15/q15.c
@@ -2,40 +2,51 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void order(float *num,int n);
-int comp_grow(float *num1,float *num2);
+void order(float *num,int size,float (*compara)(float num1,float num2));
+float comp_grow(float num1,float num2);
 
 int main(void) {
-  unsigned int n,menor;
-  float *valores;
+    unsigned int n,menor;
+    float *valores;
 
-  do{
-    printf("Quantos valores deseja ordenar: \n");
-    scanf("%d", &n);
-  }while(n==0);
+    do{
+        printf("Quantos valores deseja ordenar: \n");
+        scanf("%d", &n);
+    }while(n==0);
 
-  valores = malloc(n*sizeof(int));
+    valores = malloc(n*sizeof(float));
 
-  for(int i = 0; i < n; i++) {
-    printf("\nDigite o valor da posicao %d:", i);
-    scanf("%f", &valores[i]);
-  }
-  myqsort(valores,n,sizeof(float),comp_grow);
+    for(int i = 0; i < n; i++) {
+        printf("\nDigite o valor da posicao %d:", i);
+        scanf("%f", &valores[i]);
+    }
+    order(valores,n,comp_grow);
 
-  for(int j=0;j<n;j++){
-    printf("\n%.2f ", valores[j]);
-  }
-  free(valores);
-  return 0;
+    for(int j=0;j<n;j++){
+        printf("\n%f", valores[j]);
+    }
+    free(valores);
+    return 0;
 }
 
-int comp_grow(float *num1,float *num2){
-    return (*num1)-(*num2);
+void order(float *num,int size,float (*compara)(float num1,float num2)){
+    int i;
+    float aux,aux2;
+    bool troca;
+        do{
+            troca = false;
+            for (i = 0; i < size-1; i++) {
+                aux = compara(num[i],num[i+1]);
+                if (aux>0){
+                    aux2=num[i];
+                    num[i]=num[i+1];
+                    num[i+1]=aux2;
+                    troca = true;
+                }
+            }
+      }while(troca);
 }
 
-void myqsort(void *vetor,size_t n,size_t tam,int (*func_comp)(const void*,const void*)){
-
-
-
+float comp_grow(float num1,float num2){
+    return (num1)-(num2);
 }
-
